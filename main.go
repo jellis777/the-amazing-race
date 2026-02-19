5718f6110cfccd9454963f21d3e037e8507557cb
Jeff Ellis <jellis777@gmail.com>
2026-02-19 13:52:26 -0600

    feat: clean up output formatting

--- main.go
+++ main.go
@@ -18,31 +18,22 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Rows:", len(grid))
-	fmt.Println("Cols:", len(grid[0]))
-	fmt.Println("")
-
-	for _, row := range grid {
-		fmt.Println(row)
-	}
-
 	start, end, err := FindStartAndEnd(grid)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("")
-	fmt.Println("Start:", start)
-	fmt.Println("End:", end)
-	fmt.Println("")
 
 	path, err := SolveMaze(grid, start, end)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Print("Path: ")
-	for _, p := range path {
+	// Output with 1-Based Indexing
+	for i, p := range path {
+		if i > 0 {
+			fmt.Print(" ")
+		}
 		fmt.Printf("(%d, %d) ", p.R+1, p.C+1)
 	}
+	fmt.Println()
 }
